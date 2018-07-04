ac52732499a0adbaf6277c37b24d504ca763202b
ywl5320 <596353036@qq.com>
2018-07-05 01:48:22 +0800

    add example 1

---
+++ example_1/OpenGLDemo/app/src/main/java/com/ywl5320/opengldemo/WlGLSurfaceView.java
@@ -0,0 +1,18 @@
+package com.ywl5320.opengldemo;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+public class WlGLSurfaceView extends GLSurfaceView{
+
+    public WlGLSurfaceView(Context context) {
+        this(context, null);
+    }
+
+    public WlGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setEGLContextClientVersion(2);
+        setRenderer(new WlRender());
+    }
+}
---
+++ example_1/OpenGLDemo/app/src/main/java/com/ywl5320/opengldemo/WlRender.java
@@ -0,0 +1,26 @@
+package com.ywl5320.opengldemo;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class WlRender implements GLSurfaceView.Renderer{
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    }
+}
